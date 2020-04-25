77000df6c59b64d3a918581d1d7f0e18c10866e8
tatjama <tanja120a@gmail.com>
2020-04-25 07:12:01 +0200

    Updates

---
+++ precache-manifest.c36b6996c94013a0623c5de317c5acf8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca3291dd41ea7c9075b4e60e19f1cbae",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "292fdb3fffe245f321d1",
+    "url": "/react-tic-tac-toe/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7c9924a9f4ac84ed552b",
+    "url": "/react-tic-tac-toe/static/js/2.65a91f56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tic-tac-toe/static/js/2.65a91f56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "292fdb3fffe245f321d1",
+    "url": "/react-tic-tac-toe/static/js/main.28dde604.chunk.js"
+  },
+  {
+    "revision": "8120593895b587a0e0ee",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.fdd104c8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-tic-tac-toe/static/media/logo.5d5d9eef.svg"
+  }
+]);
